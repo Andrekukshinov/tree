a590dcff4358ed57a36e89baf3fb7126a3d296c6
Андрей <andrekukshinov@gmail.com>
2020-11-09 00:23:42 +0300

    tests refactored

--- src/test/java/by/kukshinov/tree/appliction/data/parser/ParserTest.java
+++ src/test/java/by/kukshinov/tree/appliction/data/parser/ParserTest.java
@@ -4,11 +4,12 @@ import by.kukshinov.tree.appliction.data.ExpressionRecognizer;
 import by.kukshinov.tree.appliction.model.Component;
 import by.kukshinov.tree.appliction.model.CompositeComponent;
 import by.kukshinov.tree.appliction.model.LexemeComponent;
-import by.kukshinov.tree.appliction.model.LexemeType;
 import org.mockito.Mockito;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,45 +19,58 @@ import static org.mockito.Mockito.when;
 public class ParserTest {
     private static final String SENTENCE_FOR_SPLITTING = " A [4 5+] years.";
     private static final String PARAGRAPH_FOR_SPLITTING = " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.";
-    private static final String TEXT_FOR_SPLITTING = "" +
-		  " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" +
-		  " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" +
-		  " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" +
-		  " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n";
+    private static final String TEXT_FOR_SPLITTING = "" + " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" + " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" + " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n" + " A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years. A [4 5+] years.\n";
 
-    @Test
-    public void testParseShouldParseGivenSentenceToComponent() {
-	   ExpressionRecognizer recognizer = new ExpressionRecognizer();
-	   SentenceParser parser = new SentenceParser(recognizer);
-	   LexemeComponent a = LexemeComponent.word( " A");
-	   LexemeComponent space = LexemeComponent.word( " ");
-	   LexemeComponent expression = LexemeComponent.expression( "[4 5+]");
-	   LexemeComponent years = LexemeComponent.word( " years");
-	   LexemeComponent dot = LexemeComponent.word( ".");
-	   List<Component> lexemes = Arrays.asList(a, space, expression, years, dot);
-	   Component expected = new CompositeComponent(lexemes);
-
-	   Component parsed = parser.parse(SENTENCE_FOR_SPLITTING);
-
-	   Assert.assertEquals(parsed, expected);
-    }
-
-    @Test
-    public void testParseShouldParseGivenParagraphToComponent() {
-	   SentenceParser successor = Mockito.mock(SentenceParser.class);
-	   ParagraphParser parser = new ParagraphParser(successor);
-	   LexemeComponent a = LexemeComponent.word( " A");
-	   LexemeComponent space = LexemeComponent.word( " ");
-	   LexemeComponent expression = LexemeComponent.expression( "[4 5+]");
-	   LexemeComponent years = LexemeComponent.word( " years");
-	   LexemeComponent dot = LexemeComponent.word( ".");
+    @DataProvider
+    public Object[][] getComponentWithExpression(Method method) {
+	   Object[][] result;
+	   LexemeComponent a = LexemeComponent.word(" A");
+	   LexemeComponent space = LexemeComponent.word(" ");
+	   LexemeComponent expression = LexemeComponent.expression("[4 5+]");
+	   LexemeComponent years = LexemeComponent.word(" years");
+	   LexemeComponent dot = LexemeComponent.word(".");
 	   List<Component> lexemes = Arrays.asList(a, space, expression, years, dot);
 	   Component sentence = new CompositeComponent(lexemes);
-	   when(successor.parse(anyString())).thenReturn(sentence);
 	   List<Component> sentences = Arrays
 			 .asList(sentence, sentence, sentence, sentence, sentence, sentence,
 				    sentence);
-	   Component expected = new CompositeComponent(sentences);
+	   Component paragraph = new CompositeComponent(sentences);
+	   List<Component> paragraphs = Arrays
+			 .asList(paragraph, paragraph, paragraph, paragraph);
+	   Component expected = new CompositeComponent(paragraphs);
+	   String methodName = method.getName();
+	   if ("testParseShouldParseGivenSentenceToComponent".equalsIgnoreCase(methodName)) {
+		  result = new Object[1][1];
+		  result[0][0] = sentence;
+		  return result;
+	   }
+	   if ("testParseShouldParseGivenParagraphToComponent".equalsIgnoreCase(methodName)) {
+		  result = new Object[1][2];
+		  result[0][0] = paragraph;
+		  result[0][1] = sentence;
+		  return result;
+	   }
+//	   if ("testParseShouldParseGivenTextToComponent".equalsIgnoreCase(methodName)) {
+	   result = new Object[1][2];
+	   result[0][0] = expected;
+	   result[0][1] = paragraph;
+	   return result;
+
+    }
+
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testParseShouldParseGivenSentenceToComponent(Component expected) {
+	   ExpressionRecognizer recognizer = new ExpressionRecognizer();
+	   SentenceParser parser = new SentenceParser(recognizer);
+	   Component parsed = parser.parse(SENTENCE_FOR_SPLITTING);
+	   Assert.assertEquals(parsed, expected);
+    }
+
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testParseShouldParseGivenParagraphToComponent (Component expected, Component sentence) {
+	   SentenceParser successor = Mockito.mock(SentenceParser.class);
+	   ParagraphParser parser = new ParagraphParser(successor);
+	   when(successor.parse(anyString())).thenReturn(sentence);
 
 
 	   Component parsed = parser.parse(PARAGRAPH_FOR_SPLITTING);
@@ -64,29 +78,14 @@ public class ParserTest {
 	   Assert.assertEquals(parsed, expected);
     }
 
-    @Test
-    public void testParseShouldParseGivenTextToComponent() {
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testParseShouldParseGivenTextToComponent(Component expected, Component paragraph) {
 	   ParagraphParser successor = Mockito.mock(ParagraphParser.class);
 	   TextParser parser = new TextParser(successor);
-	   LexemeComponent a = LexemeComponent.word( " A");
-	   LexemeComponent space = LexemeComponent.word( " ");
-	   LexemeComponent expression = LexemeComponent.expression( "[4 5+]");
-	   LexemeComponent years = LexemeComponent.word( " years");
-	   LexemeComponent dot = LexemeComponent.word( ".");
-	   List<Component> lexemes = Arrays.asList(a, space, expression, years, dot);
-	   Component sentence = new CompositeComponent(lexemes);
-	   List<Component> sentences = Arrays
-			 .asList(sentence, sentence, sentence, sentence, sentence, sentence,
-				    sentence);
-	   Component paragraph = new CompositeComponent(sentences);
 	   when(successor.parse(anyString())).thenReturn(paragraph);
-	   List<Component> paragraphs = Arrays
-			 .asList(paragraph, paragraph, paragraph, paragraph);
-	   Component expected = new CompositeComponent(paragraphs);
-
 	   Component parsed = parser.parse(TEXT_FOR_SPLITTING);
 
 	   Assert.assertEquals(parsed, expected);
     }
 }
-// TODO: 07.11.2020 create data provider
+// TODO: 09.11.2020 refactor 2 data providers into 1
--- src/test/java/by/kukshinov/tree/appliction/logics/ComponentProcessorTest.java
+++ src/test/java/by/kukshinov/tree/appliction/logics/ComponentProcessorTest.java
@@ -6,159 +6,154 @@ import by.kukshinov.tree.appliction.model.CompositeComponent;
 import by.kukshinov.tree.appliction.model.LexemeComponent;
 import org.mockito.Mockito;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 
 public class ComponentProcessorTest {
-    private static final String TEXT_FOR_SPLITTING = "" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n";
+    private static final String TEXT_FOR_SPLITTING = " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n" + " A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years. A 36 years.\n";
+    public static final String LEXEME_EXPRESSION = "[4 7 * 8 +]";
+    public static final String EXPRESSION = "4 7 * 8 +";
+    public static final String EXPRESSION_RESULT = "36";
 
-    @Test
-    public void testCalculateExpressionsShouldParseGivenComponentAndCalculateAllExpressions() {
+    private Component getRootComponent(List<Component> expectedLexemes) {
+	   Component expectedSentence = new CompositeComponent(expectedLexemes);
+	   List<Component> expectedSentences = Arrays
+			 .asList(expectedSentence, expectedSentence, expectedSentence,
+				    expectedSentence, expectedSentence, expectedSentence,
+				    expectedSentence);
+	   Component expectedParagraph = new CompositeComponent(expectedSentences);
+	   List<Component> expectedParagraphs = Arrays
+			 .asList(expectedParagraph, expectedParagraph, expectedParagraph,
+				    expectedParagraph);
+	   return new CompositeComponent(expectedParagraphs);
+    }
+
+    @DataProvider
+    public Object[][] getComponentWithExpression(Method method) {
+	   Object[][] result;
+	   LexemeComponent a = LexemeComponent.word(" A");
+	   LexemeComponent space = LexemeComponent.word(" ");
+	   LexemeComponent expression = LexemeComponent.expression("[4 5+]");
+	   LexemeComponent years = LexemeComponent.word(" years");
+	   LexemeComponent dot = LexemeComponent.word(".");
+	   LexemeComponent nine = LexemeComponent.word("36");
+
+	   LexemeComponent aBlank = LexemeComponent.word(" A ");
+	   LexemeComponent spaceBlank = LexemeComponent.word(" ");
+	   LexemeComponent nineBlank = LexemeComponent.word(" 36 ");
+	   LexemeComponent yearsBlank = LexemeComponent.word(" years");
+	   LexemeComponent dotBlank = LexemeComponent.word(". ");
+
+	   List<Component> lexemesExpression = Arrays.asList(a, space, expression, years, dot);
+	   Component expectedExpression = getRootComponent(lexemesExpression);
+
+	   List<Component> lexemesNine = Arrays.asList(a, space, nine, years, dot);
+	   Component startNine = getRootComponent(lexemesNine);
+
+	   Component sentenceNine = new CompositeComponent(lexemesNine);
+	   List<Component> sentencesNines1 = Arrays
+			 .asList(sentenceNine, sentenceNine, sentenceNine,
+				    sentenceNine);
+	   List<Component> sentencesNines2 = Arrays
+			 .asList(sentenceNine, sentenceNine);
+	   List<Component> sentencesNines3 = Arrays
+			 .asList(sentenceNine, sentenceNine, sentenceNine);
+
+	   Component paragraphNine1 = new CompositeComponent(sentencesNines1);
+	   Component paragraphNine2 = new CompositeComponent(sentencesNines2);
+	   Component paragraphNine3 = new CompositeComponent(sentencesNines3);
+	   List<Component> paragraphsForSorting = Arrays.asList(paragraphNine1, paragraphNine2, paragraphNine3);
+	   List<Component> paragraphNines = Arrays.asList(paragraphNine2, paragraphNine3, paragraphNine1);
+	   Component paragraphsStarting = new CompositeComponent(paragraphsForSorting);
+	   Component paragraphsNines = new CompositeComponent(paragraphNines);
+
+	   List<Component> expectedLexemesWithBlanks = Arrays.asList(spaceBlank, dotBlank, aBlank, nineBlank ,yearsBlank);
+	   List<Component> startLexemesWithBlanks = Arrays.asList(aBlank, spaceBlank, nineBlank ,yearsBlank, dotBlank);
+	   Component expectedExpressionWithBlanks = getRootComponent(expectedLexemesWithBlanks);
+	   Component startExpressionWithBlanks = getRootComponent(startLexemesWithBlanks);
+
+	   String methodName = method.getName();
+	   if ("testCalculateExpressionsShouldParseGivenComponentAndCalculateAllExpressions"
+			 .equalsIgnoreCase(methodName)) {
+		  result = new Object[1][2];
+		  result[0][0] = expectedExpression;
+		  result[0][1] = expectedExpression;
+		  return result;
+	   }
+	   if ("testRestoreTextShouldRestoreTextFromComponent".equalsIgnoreCase(methodName)) {
+		  result = new Object[1][1];
+		  result[0][0] = startNine;
+		  return result;
+	   }
+	   if ("testSortParagraphsByLengthShouldSortParagraphsInComponentBySentenceLength".equalsIgnoreCase(methodName)) {
+		  result = new Object[1][2];
+		  result[0][0] = paragraphsStarting;
+		  result[0][1] = paragraphsNines;
+		  return result;
+	   }
+//	   if ("testSortSentenceByLexemeLengthShouldSortSentencesInComponentByLexemeLength".equalsIgnoreCase(methodName)) {
+		  result = new Object[1][2];
+		  result[0][0] = startExpressionWithBlanks;
+		  result[0][1] = expectedExpressionWithBlanks;
+		  return result;
+//	   }
+    }
+
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testCalculateExpressionsShouldParseGivenComponentAndCalculateAllExpressions(
+		  Component start, Component expected) {
 	   ExpressionRecognizer recognizer = Mockito.mock(ExpressionRecognizer.class);
 	   Interpreter interpreter = Mockito.mock(Interpreter.class);
 	   ComponentProcessor componentProcessor = new ComponentProcessor(recognizer,
 			 interpreter);
-	   LexemeComponent a = LexemeComponent.word(" A");
-	   LexemeComponent space = LexemeComponent.word(" ");
-	   LexemeComponent expression = LexemeComponent.expression("[4 7 * 8 +]");
-	   LexemeComponent nine = LexemeComponent.word("36");
-	   LexemeComponent years = LexemeComponent.word(" years");
-	   LexemeComponent dot = LexemeComponent.word(".");
 
-	   List<Component> expectedLexemes = Arrays.asList(a, space, nine, years, dot);
-	   Component expectedSentence = new CompositeComponent(expectedLexemes);
-	   List<Component> expectedSentences = Arrays
-			 .asList(expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence);
-	   Component expectedParagraph = new CompositeComponent(expectedSentences);
-	   List<Component> expectedParagraphs = Arrays
-			 .asList(expectedParagraph, expectedParagraph, expectedParagraph,
-				    expectedParagraph);
-	   Component expected = new CompositeComponent(expectedParagraphs);
-
-
-	   List<Component> startLexemes = Arrays.asList(a, space, expression, years, dot);
-	   Component startSentence = new CompositeComponent(startLexemes);
-	   List<Component> startSentences = Arrays
-			 .asList(startSentence, startSentence, startSentence, startSentence,
-				    startSentence, startSentence, startSentence);
-	   Component startParagraph = new CompositeComponent(startSentences);
-	   List<Component> startParagraphs = Arrays
-			 .asList(startParagraph, startParagraph, startParagraph, startParagraph);
-	   Component start = new CompositeComponent(startParagraphs);
-
-	   when(recognizer.isExpression("[8 2 7 4 + * -]")).thenReturn(true);
-	   when(recognizer.isExpression("[4 7 * 8 +]")).thenReturn(true);
-
-	   when(interpreter.calculate("[8 2 7 4 + * -]")).thenReturn("14");
-	   when(interpreter.calculate("4 7 * 8 +")).thenReturn("36");
+	   when(recognizer.isExpression(LEXEME_EXPRESSION)).thenReturn(true);
+	   when(interpreter.calculate(EXPRESSION)).thenReturn(EXPRESSION_RESULT);
 
 	   Component actual = componentProcessor.calculateExpressions(start);
 
-	   Assert.assertEquals(actual, expected);// TODO: 07.11.2020 refactor tests
+	   Assert.assertEquals(actual, expected);
     }
 
-
-    @Test
-    public void testRestoreTextShouldRestoreTextFromComponent() {
-	   ComponentProcessor componentProcessor = new ComponentProcessor(null, null);
-	   LexemeComponent a = LexemeComponent.word(" A");
-	   LexemeComponent space = LexemeComponent.word(" ");
-	   LexemeComponent nine = LexemeComponent.word("36");
-	   LexemeComponent years = LexemeComponent.word(" years");
-	   LexemeComponent dot = LexemeComponent.word(".");
-
-	   List<Component> expectedLexemes = Arrays.asList(a, space, nine, years, dot);
-	   Component expectedSentence = new CompositeComponent(expectedLexemes);
-	   List<Component> expectedSentences = Arrays
-			 .asList(expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence);
-	   Component expectedParagraph = new CompositeComponent(expectedSentences);
-	   List<Component> expectedParagraphs = Arrays
-			 .asList(expectedParagraph, expectedParagraph, expectedParagraph,
-				    expectedParagraph);
-	   Component restoreFrom = new CompositeComponent(expectedParagraphs);
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testRestoreTextShouldRestoreTextFromComponent(Component restoreFrom) {
+	   ExpressionRecognizer recognizer = Mockito.mock(ExpressionRecognizer.class);
+	   Interpreter interpreter = Mockito.mock(Interpreter.class);
+	   ComponentProcessor componentProcessor = new ComponentProcessor(recognizer,
+			 interpreter);
 	   String actual = componentProcessor.restoreText(restoreFrom);
 
-	   Assert.assertEquals(actual, TEXT_FOR_SPLITTING);// TODO: 07.11.2020 refactor tests
+	   Assert.assertEquals(actual, TEXT_FOR_SPLITTING);
     }
-    @Test
-    public void testSortParagraphsByLengthShouldSortParagraphsInComponentBySentenceLength() {
-	   ComponentProcessor componentProcessor = new ComponentProcessor(null, null);
-	   LexemeComponent a = LexemeComponent.word(" A");
-	   LexemeComponent space = LexemeComponent.word(" ");
-	   LexemeComponent nine = LexemeComponent.word("36");
-	   LexemeComponent years = LexemeComponent.word(" years");
-	   LexemeComponent dot = LexemeComponent.word(".");
 
-	   List<Component> expectedLexemes = Arrays.asList(a, space, nine, years, dot);
-	   Component expectedSentence = new CompositeComponent(expectedLexemes);
-
-	   List<Component> expectedSentences1 = Arrays
-			 .asList(expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence);
-	   List<Component> expectedSentences2 = Arrays
-			 .asList(expectedSentence, expectedSentence);
-	   List<Component> expectedSentences3 = Arrays
-			 .asList(expectedSentence, expectedSentence, expectedSentence);
-
-	   Component expectedParagraph1 = new CompositeComponent(expectedSentences1);
-	   Component expectedParagraph2 = new CompositeComponent(expectedSentences2);
-	   Component expectedParagraph3 = new CompositeComponent(expectedSentences3);
-	   List<Component> paragraphsForSorting = Arrays.asList(expectedParagraph1, expectedParagraph2, expectedParagraph3);
-	   List<Component> expectedParagraphs = Arrays.asList(expectedParagraph2, expectedParagraph3, expectedParagraph1);
-	   Component forSorting = new CompositeComponent(paragraphsForSorting);
-	   Component expected = new CompositeComponent(expectedParagraphs);
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testSortParagraphsByLengthShouldSortParagraphsInComponentBySentenceLength(Component forSorting, Component expected) {
+	   ExpressionRecognizer recognizer = Mockito.mock(ExpressionRecognizer.class);
+	   Interpreter interpreter = Mockito.mock(Interpreter.class);
+	   ComponentProcessor componentProcessor = new ComponentProcessor(recognizer,
+			 interpreter);
 	   Component actual = componentProcessor.sortParagraphsBySentenceLength(forSorting);
 
-	   Assert.assertEquals(actual, expected);// TODO: 07.11.2020 refactor tests
+	   Assert.assertEquals(actual, expected);
 
     }
-    
-    @Test
-    public void testSortSentenceByLexemeLengthShouldSortSentencesInComponentByLexemeLength() {
-	   ComponentProcessor componentProcessor = new ComponentProcessor(null, null);
-	   LexemeComponent a = LexemeComponent.word(" A ");
-	   LexemeComponent space = LexemeComponent.word(" ");
-	   LexemeComponent nine = LexemeComponent.word(" 36 ");
-	   LexemeComponent years = LexemeComponent.word(" years");
-	   LexemeComponent dot = LexemeComponent.word(". ");
 
-	   List<Component> expectedLexemes = Arrays.asList(space, dot, a, nine ,years);
-	   Component expectedSentence = new CompositeComponent(expectedLexemes);
-	   List<Component> expectedSentences = Arrays
-			 .asList(expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence, expectedSentence, expectedSentence,
-				    expectedSentence);
-	   Component expectedParagraph = new CompositeComponent(expectedSentences);
-	   List<Component> expectedParagraphs = Arrays
-			 .asList(expectedParagraph, expectedParagraph, expectedParagraph,
-				    expectedParagraph);
-	   Component expected = new CompositeComponent(expectedParagraphs);
-
-
-
-	   List<Component> startLexemes = Arrays.asList(a, space, nine, years, dot);
-	   Component startSentence = new CompositeComponent(startLexemes);
-	   List<Component> startSentences = Arrays
-			 .asList(startSentence, startSentence, startSentence,
-				    startSentence, startSentence, startSentence,
-				    startSentence);
-	   Component startParagraph = new CompositeComponent(startSentences);
-	   List<Component> startParagraphs = Arrays
-			 .asList(startParagraph, startParagraph, startParagraph,
-				    startParagraph);
-	   Component startComponent = new CompositeComponent(startParagraphs);
+    @Test(dataProvider = "getComponentWithExpression")
+    public void testSortSentenceByLexemeLengthShouldSortSentencesInComponentByLexemeLength(Component startComponent, Component expected) {
+	   ExpressionRecognizer recognizer = Mockito.mock(ExpressionRecognizer.class);
+	   Interpreter interpreter = Mockito.mock(Interpreter.class);
+	   ComponentProcessor componentProcessor = new ComponentProcessor(recognizer,
+			 interpreter);
 
 	   Component actual = componentProcessor.sortSentenceByLexemeLength(startComponent);
 
-	   Assert.assertEquals(actual, expected);// TODO: 07.11.2020 refactor tests
+	   Assert.assertEquals(actual, expected);
 
     }
 }
