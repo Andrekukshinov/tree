fc501e65c1441c5c8d3d381697575f5cf3750297
Андрей <andrekukshinov@gmail.com>
2020-11-10 15:32:22 +0300

    constants refactored

--- src/main/java/by/kukshinov/tree/appliction/logics/Interpreter.java
+++ src/main/java/by/kukshinov/tree/appliction/logics/Interpreter.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Interpreter {
-    private static final String SPACES = "\\s";
+    private static final String SPACES = "\\s+";
     private static final char PLUS = '+';
     private static final char MINUS = '-';
     private static final char MULTIPLY = '*';
