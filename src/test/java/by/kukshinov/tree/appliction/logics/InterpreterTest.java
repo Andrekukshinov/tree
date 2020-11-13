f214d1253e4c6bcdbbe2488a7857f18a2efd4f7d
Андрей <andrekukshinov@gmail.com>
2020-11-13 15:32:53 +0300

    tests refactored

--- src/test/java/by/kukshinov/tree/appliction/logics/InterpreterTest.java
+++ src/test/java/by/kukshinov/tree/appliction/logics/InterpreterTest.java
@@ -1,19 +1,44 @@
 package by.kukshinov.tree.appliction.logics;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class InterpreterTest {
-    private static final String EXPRESSION = "8 2 7 4 + * -";
-    private static final String EXPECTED = "14";
 
-    @Test
-    public void testCalculateShouldCalculateGivenExpressionToString() {
+    @DataProvider
+    public Object[][] expressionCalculatorDataProvider() {
+        String expression = "8 2 7 4 + * -";
+        String expected = "14";
+        Object[][] result = new Object[6][2];
+        result[0][0] = "4 5 +";
+        result[0][1] = "9";
+
+        result[1][0] = "5 4 -";;
+        result[1][1] = "-1";
+
+
+        result[2][0] = "4 5 *";
+        result[2][1] = "20";
+
+        result[3][0] = "5 20 /";
+        result[3][1] = "4";
+
+        result[4][0] = "4 5 -";;
+        result[4][1] = "1";
+
+        result[5][0] = expression;
+        result[5][1] = expected;
+        return result;
+    }
+
+    @Test(dataProvider = "expressionCalculatorDataProvider")
+    public void testCalculateShouldCalculateGivenExpressionToString(String expression, String result) {
 	   Interpreter interpreter = new Interpreter();
 
-	   String calculated = interpreter.calculate(EXPRESSION);
+	   String calculated = interpreter.calculate(expression);
 
-	   Assert.assertEquals(calculated, EXPECTED);
+	   Assert.assertEquals(calculated, result);
     }
 
 }
